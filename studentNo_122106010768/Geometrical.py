4e559aee6782a677160435a089ea9947866e9e76
waw <156472065@qq.com>
2022-10-21 21:41:01 +0800

    增加水平镜像

--- studentNo_122106010768/Geometrical.py
+++ studentNo_122106010768/Geometrical.py
@@ -16,13 +16,23 @@ def move(right, img):
     return result
 
 # 垂直镜像函数的实现
-def acous_tic(img):
+def h_acous_tic(img):
     imginfo = img.shape
     result = np.zeros([imginfo[0] * 2, imginfo[1], imginfo[2]], np.uint8)
+
     for i in range(imginfo[0]):
         for j in range(imginfo[1]):
             result[i, j] = img[i, j]
             result[imginfo[0] * 2 - i - 1, j] = img[i, j]
+    return result
+# 水平镜像函数的实现
+def w_acous_tic(img):
+    imginfo = img.shape
+    result = np.zeros([imginfo[0], imginfo[1] * 2, imginfo[2]], np.uint8)
+    for i in range(imginfo[0]):
+        for j in range(imginfo[1]):
+            result[i, j] = img[i, j]
+            result[i, 2*imginfo[1]-1-j] = img[i, j]
     return result
 
 # 旋转函数的视线
@@ -36,14 +46,15 @@ def matrotate(img):
 def all_function(img):
     move1 = move(150, img)
     matrotate1 = matrotate(move1)
-    return acous_tic(matrotate1)
+    return h_acous_tic(matrotate1)
 
 if __name__ == "__main__":
     img_in = cv2.imread("flower.jpg", 1)
     # 图片向右移动100个像素
     move_img = move(100, img_in)
-    # 图片垂直镜像
-    acous_tic_img = acous_tic(img_in)
+    # 图片垂直镜像 和水平镜像
+    h_acous_tic_img = h_acous_tic(img_in)
+    w_acous_tic_img = w_acous_tic(img_in)
     # 图片旋转45度
     matrotate_img = matrotate(img_in)
     # 调用三种变换的符合函数
@@ -51,18 +62,21 @@ if __name__ == "__main__":
 
     cv2.namedWindow("向右平移100", 0)
     cv2.namedWindow("垂直镜像", 0)
+    cv2.namedWindow("水平镜像", 0)
     cv2.namedWindow("中心旋转45度", 0)
     cv2.namedWindow("复合变换", 0)
 
 
     cv2.resizeWindow("向右平移100", 500, 400)
     cv2.resizeWindow("垂直镜像", 500, 400)
+    cv2.resizeWindow("水平镜像", 500, 400)
     cv2.resizeWindow("中心旋转45度", 500, 400)
     cv2.resizeWindow("复合变换", 500, 400)
 
 
     cv2.imshow("向右平移100", move_img)
-    cv2.imshow("垂直镜像", acous_tic_img)
+    cv2.imshow("垂直镜像", h_acous_tic_img)
+    cv2.imshow("水平镜像", w_acous_tic_img)
     cv2.imshow("中心旋转45度", matrotate_img)
     cv2.imshow("复合变换", last_img)
 
